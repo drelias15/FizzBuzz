be68241a622ab20d3720d77832e31a6756fac442
drelias15 <drelias15@gmail.com>
2018-07-17 15:07:17 -0400

    Updated

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,9 +4,11 @@ public class FizzBuzz {
 
         for (int i = 1;  i <= 100; i++){
 
-            if(i%3 ==0 || i%5 ==0){
+            if(i%3 ==0){
             System.out.println("Fizz");
-        }else{
+        }else if (i%5 ==0) {
+                System.out.println("Buzz");
+            }else{
                 System.out.println(i);
             }
         }
