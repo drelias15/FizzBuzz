8bbe2fb2ec4ee5219d6e07cadbfa51dca1507eb5
drelias15 <drelias15@gmail.com>
2018-07-17 15:19:48 -0400

    With FizzBuzz

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,10 +4,12 @@ public class FizzBuzz {
 
         for (int i = 1;  i <= 100; i++){
 
-            if(i%3 ==0){
-            System.out.println("Fizz");
+            if(i%5==0 && i%3==0){
+            System.out.println("FizzBuzz");
         }else if (i%5 ==0) {
                 System.out.println("Buzz");
+            }else if (i%3 ==0){
+                System.out.println("Fizz");
             }else{
                 System.out.println(i);
             }
